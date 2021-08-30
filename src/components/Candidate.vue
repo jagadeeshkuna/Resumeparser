d32456e6ce528aec710a2e9e0105861043f8ea4f
500apps-jagadeeshkunajagadeeshkuna <jagadeesh.kuna@500apps.com>
2021-08-30 18:48:53 +0530

    ui

--- src/components/Candidate.vue
+++ src/components/Candidate.vue
@@ -12,66 +12,93 @@
         /> -->
         <hr />
         <label><b>Name</b></label>
-        <input
-          type="text"
+        <div>
+              <b-form-input
+      class="mb-2 mr-sm-2 mb-sm-0"
+       type="text"
           placeholder="Enter Your Name"
           name="name"
           id="name"
           required
-        />
+    ></b-form-input>
+        </div>
         <label><b>Email</b></label>
-        <input
-          type="email"
+        <div>
+          <b-form-input
+      class="mb-2 mr-sm-2 mb-sm-0"
+       type="email"
           placeholder="Enter Your Email"
           name="email"
           id="email"
           required
-        />
-
+    ></b-form-input>
+</div>
+ <br>
         <label><b>Contact</b></label>
-        <input
-          type="number"
+<div>
+          <b-form-input
+      class="mb-2 mr-sm-2 mb-sm-0"
+       type="number"
           placeholder="Enter Contact Number"
           name="contact"
           id="contact"
-          required
-        />
+          required>
+    </b-form-input>
+        </div>
         <label><b>Date of birth</b></label>
-        <input
+        <div>
+          <b-form-input
           type="date"
           placeholder="Enter date of birth"
           name="dob"
           id="dob"
-        />
+          required>
+    </b-form-input>
+        </div>
         <label><b>Address</b></label>
-        <textarea
+        <div>
+          <b-form-input
+      class="mb-2 mr-sm-2 mb-sm-0"
           placeholder="Enter your address"
           name="address"
           id="address"
-        />
+          required>
+    </b-form-input>
+        </div>
         <label><b>Experience</b></label>
-        <input
-          type="number"
+        <div>
+            <b-form-input
+           type="number"
           placeholder="Enter Your Experience"
           name="experience"
           id="experience"
-        />
+        >
+          </b-form-input>
+        </div>
         <label><b>Skills</b></label>
-        <textarea
-          type="text"
+        <div>
+           <b-form-input
+         type="text"
           placeholder="Enter Your Skills"
           name="skills"
           id="skills"
-        />
+        >
+       </b-form-input>
+        </div>
+
         <label><b>Education</b></label>
-        <textarea
-          type="text"
+        <div>
+           <b-form-input
+        type="text"
           placeholder="Enter Education Details"
           name="education"
           id="education"
-        />
-
-        <button type="submit" class="submitbtn">Submit</button>
+        >
+       </b-form-input>
+</div>
+<div class="mt-3">
+   <b-button pill variant="primary"  type="submit" class="submitbtn text-right mt-3">Button</b-button>
+</div>
       </div>
     </form>
   </div>
@@ -82,45 +109,3 @@ export default {
   name: "Candidate",
 };
 </script>
-<style scoped>
-* {
-  box-sizing: border-box;
-}
-
-/* Add padding to containers */
-.container {
-  padding: 16px;
-}
-
-/* Full-width input fields */
-input,
-textarea {
-  width: 100%;
-  padding: 10px;
-  margin: 5px 0 22px 0;
-  display: inline-block;
-}
-
-/* Set a style for the submit/register button */
-.submitbtn {
-  background-color: #04aa6d;
-  color: white;
-  padding: 16px 20px;
-  margin: 8px 0;
-  border: none;
-  cursor: pointer;
-  width: 100%;
-  opacity: 0.9;
-}
-
-/* Add a blue text color to links */
-a {
-  color: dodgerblue;
-}
-
-/* Set a grey background color and center the text of the "sign in" section */
-.signin {
-  background-color: #f1f1f1;
-  text-align: center;
-}
-</style>
